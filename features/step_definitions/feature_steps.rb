a6ecc1dffc7650a7d645e794340b25fcd1a9c46b
nadia109 <ngrodrig@g.cofc.edu>
2014-11-11 00:24:41 -0400

    cucumber still has an error

--- features/step_definitions/feature_steps.rb
+++ features/step_definitions/feature_steps.rb
@@ -1,15 +1,9 @@
 
 Given /the following workouts exist/ do |workouts_table|
-  Workouts_table.hashes.each do |workout|
-   Workout.create!(workout)
-  end
   flunk "Unimplemented"
 end
 
 Given /the following profiles exist/ do |profiles_table|
-  Profiles_table.hashes.each do |profile|
-   Profile.create!(profile)
-  end
   flunk "Unimplemented"
 end
 
@@ -17,11 +11,11 @@ Given /the following workoutlogs exist/ do |workoutLogs_table|
   flunk "Unimplemented"
 end
 
-Given /I am logged in as (*.)/ do |email|
+Given /I am logged in as (.*)/ do |email|
 	flunk "Unimplemented"
 end
 
-When /I enter all my information on '(*.)' page/ do |pagename|
+When /I enter all my information on '(.*)' page/ do |pagename|
 	flunk "Unimplemented"
 end
 
@@ -29,15 +23,7 @@ Then /I should see all the workouts/ do
   flunk "Unimplemented"
 end
 
-Then /I should see '(*.)'/ do |text|
-  flunk "Unimplemented"
-end
-
-Then /I should not see '(*.)'/ do |text|
-  flunk "Unimplemented"
-end
-
-Then /I should be redirected to the '(*.)' page/ do |pagename|
+Then /I should be redirected to the '(.*)' page/ do |pagename|
 	flunk "Unimplemented"
 end
 
